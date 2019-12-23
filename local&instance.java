cbd268b5a2129482a248d46e6fbbf85c089157b5
SIDTATUL <19103073@iubat.edu>
2019-12-23 19:50:31 +0600

    comment added

--- local&instance.java
+++ local&instance.java
@@ -14,5 +14,31 @@ class poorDog{
         poorDog one = new poorDog();
         System.out.println("Size : " + one.getSize());
         System.out.println("Name : " + one.getName());
+        //it will not run if we not initial value to local variable
+        /* int x ;
+        System.out.println(x); */
+        /* Local variables do
+        NOT get a default
+        value! The compiler
+        complains if you
+        try to use a local
+        variable before
+        the variable is
+        initialized. */
+
+        /* Instance variables
+        always get a
+        default value. If
+        you don't explicitly
+        assign a value
+        to an instance
+        variable, or you
+        don't call a setter
+        method, the
+        instance variab'e
+        still has a value!
+        integers 0
+        floating points 0.0
+        booleans false */
     }
 }
